a8999a76b0ae00cf4ec3f754adafbcbc5fffcaa7
cantuananh <tuananhthemen391997@gmail.com>
2025-10-03 15:35:40 +0700

    edit person feature

--- Person.java
+++ Person.java
@@ -2,6 +2,7 @@ public class Person {
     public static void main(String[] args) {
         System.out.println("Hello, I'm Main");
         System.out.println("Add new person successfully");
+        System.out.println("Update information person successfully");
     }
 }
 
