21186a4ff62d0419d8a810d86d36455c798e46f9
cantuananh <tuananhthemen391997@gmail.com>
2025-10-03 15:32:13 +0700

    add new person feature

--- Person.java
+++ Person.java
@@ -1,6 +1,7 @@
 public class Person {
     public static void main(String[] args) {
         System.out.println("Hello, I'm Main");
+        System.out.println("Add new person successfully");
     }
 }
 
